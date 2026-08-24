780b7e891e04d572c32d4cb7a1bdcb8e71b2e2ac
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-08-24 21:39:05 +0530

    feat(workouts): register exercise history module components

--- src/workouts/workouts.module.ts
+++ src/workouts/workouts.module.ts
@@ -6,10 +6,23 @@ import { WorkoutAssignmentsService } from './workout-assignments.service';
 import { WorkoutExecutionService } from './workout-execution.service';
 import { WorkoutPlansController } from './workout-plans.controller';
 import { WorkoutPlansService } from './workout-plans.service';
+import { ExerciseHistoryController } from './exercise-history.controller';
+import { ExerciseHistoryService } from './exercise-history.service';
 
 @Module({
-  controllers: [ExercisesController, WorkoutPlansController, WorkoutAssignmentsController],
-  providers: [ExercisesService, WorkoutPlansService, WorkoutAssignmentsService, WorkoutExecutionService],
-  exports: [ExercisesService, WorkoutPlansService, WorkoutAssignmentsService, WorkoutExecutionService],
+  controllers: [
+    ExercisesController,
+    WorkoutPlansController,
+    WorkoutAssignmentsController,
+    ExerciseHistoryController,
+  ],
+  providers: [
+    ExercisesService,
+    WorkoutPlansService,
+    WorkoutAssignmentsService,
+    WorkoutExecutionService,
+    ExerciseHistoryService,
+  ],
+  exports: [ExercisesService, WorkoutPlansService, WorkoutAssignmentsService, WorkoutExecutionService, ExerciseHistoryService],
 })
 export class WorkoutsModule {}
