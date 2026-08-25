07415d0018f2a80accfe2418269c60895c4b7369
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-08-25 05:45:22 +0530

    fix: enforce trainer assignment scope on membership reads

--- src/memberships/memberships.controller.ts
+++ src/memberships/memberships.controller.ts
@@ -1,8 +1,9 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { Audited } from '../common/decorators/audited.decorator';
+import { CurrentAssignmentScope } from '../common/decorators/assignment-scope.decorator';
 import { CurrentBranchScope } from '../common/decorators/branch-scope.decorator';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
-import { RequirePermissions } from '../common/decorators/permissions.decorator';
+import { RequireAnyPermission, RequirePermissions } from '../common/decorators/permissions.decorator';
 import type { AuthenticatedUser } from '../common/types/authenticated-user';
 import { CancelMembershipDto } from './dto/cancel-membership.dto';
 import { CreateMembershipDto } from './dto/create-membership.dto';
@@ -15,31 +16,28 @@ export class MembershipsController {
   constructor(private readonly membershipsService: MembershipsService) {}
 
   @Get()
-  @RequirePermissions('memberships.read')
+  @RequireAnyPermission('memberships.read', 'memberships.read_assigned')
   list(
     @CurrentUser() user: AuthenticatedUser,
     @Query() query: ListMembershipsQueryDto,
     @CurrentBranchScope() branchScope: string | null,
+    @CurrentAssignmentScope() assignmentScope: string | null,
   ) {
     return this.membershipsService.list(
-      user.organizationId!,
-      query,
-      query.memberId,
-      branchScope,
+      user.organizationId!, query, query.memberId, branchScope, assignmentScope,
     );
   }
 
   @Get(':id')
-  @RequirePermissions('memberships.read')
+  @RequireAnyPermission('memberships.read', 'memberships.read_assigned')
   getOne(
     @CurrentUser() user: AuthenticatedUser,
     @Param('id') id: string,
     @CurrentBranchScope() branchScope: string | null,
+    @CurrentAssignmentScope() assignmentScope: string | null,
   ) {
     return this.membershipsService.getOne(
-      user.organizationId!,
-      id,
-      branchScope,
+      user.organizationId!, id, branchScope, assignmentScope,
     );
   }
 
@@ -51,11 +49,7 @@ export class MembershipsController {
     @Body() dto: CreateMembershipDto,
     @CurrentBranchScope() branchScope: string | null,
   ) {
-    return this.membershipsService.create(
-      user.organizationId!,
-      dto,
-      branchScope,
-    );
+    return this.membershipsService.create(user.organizationId!, dto, branchScope);
   }
 
   @Post(':id/freeze')
@@ -67,12 +61,7 @@ export class MembershipsController {
     @Body() dto: FreezeMembershipDto,
     @CurrentBranchScope() branchScope: string | null,
   ) {
-    return this.membershipsService.freeze(
-      user.organizationId!,
-      id,
-      dto,
-      branchScope,
-    );
+    return this.membershipsService.freeze(user.organizationId!, id, dto, branchScope);
   }
 
   @Post(':id/resume')
@@ -83,11 +72,7 @@ export class MembershipsController {
     @Param('id') id: string,
     @CurrentBranchScope() branchScope: string | null,
   ) {
-    return this.membershipsService.resume(
-      user.organizationId!,
-      id,
-      branchScope,
-    );
+    return this.membershipsService.resume(user.organizationId!, id, branchScope);
   }
 
   @Post(':id/cancel')
@@ -99,11 +84,6 @@ export class MembershipsController {
     @Body() dto: CancelMembershipDto,
     @CurrentBranchScope() branchScope: string | null,
   ) {
-    return this.membershipsService.cancel(
-      user.organizationId!,
-      id,
-      dto,
-      branchScope,
-    );
+    return this.membershipsService.cancel(user.organizationId!, id, dto, branchScope);
   }
 }
