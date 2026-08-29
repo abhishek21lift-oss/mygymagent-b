c2f03524d77ca223acabdcb4d53b9ac69a55bd5c
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-08-29 15:49:47 +0530

    feat: add PT package create DTO

---
+++ src/pt-packages/dto/create-pt-package.dto.ts
@@ -0,0 +1,13 @@
+import { IsDateString, IsInt, IsNumber, IsOptional, IsString, IsUUID, Min } from 'class-validator';
+
+export class CreatePtPackageDto {
+  @IsUUID() memberId!: string;
+  @IsUUID() branchId!: string;
+  @IsOptional() @IsUUID() templateId?: string;
+  @IsString() name!: string;
+  @IsInt() @Min(1) totalSessions!: number;
+  @IsDateString() startDate!: string;
+  @IsDateString() endDate!: string;
+  @IsNumber() @Min(0) price!: number;
+  @IsOptional() @IsString() currency?: string;
+}
