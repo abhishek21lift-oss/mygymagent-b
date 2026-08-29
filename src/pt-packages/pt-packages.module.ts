2c3909ff1a0719509e3a2f8bfd9f165725c6ce09
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-08-29 15:50:00 +0530

    feat: add PT packages module

---
+++ src/pt-packages/pt-packages.module.ts
@@ -0,0 +1,7 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { PtPackagesController } from './pt-packages.controller';
+import { PtPackagesService } from './pt-packages.service';
+
+@Module({ controllers: [PtPackagesController], providers: [PtPackagesService, PrismaService], exports: [PtPackagesService] })
+export class PtPackagesModule {}
