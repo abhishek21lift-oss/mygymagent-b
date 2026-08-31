a0b9c667b9b712907db73cc95b4fb46fe1c75466
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-08-31 07:05:45 +0530

    fix(pt): align package controller with current auth types

--- src/pt-packages/pt-packages.controller.ts
+++ src/pt-packages/pt-packages.controller.ts
@@ -1,37 +1,29 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
 import { RequirePermissions } from '../common/decorators/permissions.decorator';
-import type { CurrentUser as CurrentUserType } from '../common/types/authenticated-user';
+import type { AuthenticatedUser } from '../common/types/authenticated-user';
 import { PtPackagesService } from './pt-packages.service';
 import { CreatePtPackageDto } from './dto/create-pt-package.dto';
 
-@ApiTags('pt-packages')
 @Controller('pt-packages')
 export class PtPackagesController {
   constructor(private readonly service: PtPackagesService) {}
 
   @Get()
   @RequirePermissions('pt-packages.read')
-  list(
-    @CurrentUser() user: CurrentUserType,
-    @Query('memberId') memberId?: string,
-  ) {
+  list(@CurrentUser() user: AuthenticatedUser, @Query('memberId') memberId?: string) {
     return this.service.list(user.organizationId, memberId);
   }
 
   @Get(':id')
   @RequirePermissions('pt-packages.read')
-  getOne(@Param('id') id: string, @CurrentUser() user: CurrentUserType) {
+  getOne(@Param('id') id: string, @CurrentUser() user: AuthenticatedUser) {
     return this.service.getOne(user.organizationId, id);
   }
 
   @Post()
   @RequirePermissions('pt-packages.create')
-  create(
-    @Body() dto: CreatePtPackageDto,
-    @CurrentUser() user: CurrentUserType,
-  ) {
+  create(@Body() dto: CreatePtPackageDto, @CurrentUser() user: AuthenticatedUser) {
     return this.service.create(user.organizationId, dto, user.id);
   }
 }
