641994fd8e1685c4327fe7a30fa7fc68ba5a06e7
abhishek <abhishek@example.com>
2026-09-06 04:19:59 +0530

    Fix PtSessionsModule: import MembersService dependency

--- src/pt-sessions/pt-sessions.module.ts
+++ src/pt-sessions/pt-sessions.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { PrismaService } from '../prisma/prisma.service';
 import { PtPackagesModule } from '../pt-packages/pt-packages.module';
+import { MembersModule } from '../members/members.module';
 import { PtSessionsController } from './pt-sessions.controller';
 import { PtSessionsService } from './pt-sessions.service';
 
 @Module({
-  imports: [EventEmitterModule, PtPackagesModule],
+  imports: [EventEmitterModule, PtPackagesModule, MembersModule],
   controllers: [PtSessionsController],
   providers: [PtSessionsService, PrismaService],
   exports: [PtSessionsService],
