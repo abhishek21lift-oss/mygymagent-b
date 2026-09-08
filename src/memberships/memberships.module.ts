5debff4c81405aef9e0f95433c407f532271811d
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-08 12:25:11 +0530

    feat(memberships): register lifecycle service

--- src/memberships/memberships.module.ts
+++ src/memberships/memberships.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MembershipsController } from './memberships.controller';
 import { MembershipsService } from './memberships.service';
+import { MembershipLifecycleService } from './membership-lifecycle.service';
 
 @Module({
   controllers: [MembershipsController],
-  providers: [MembershipsService],
-  exports: [MembershipsService],
+  providers: [MembershipsService, MembershipLifecycleService],
+  exports: [MembershipsService, MembershipLifecycleService],
 })
 export class MembershipsModule {}
