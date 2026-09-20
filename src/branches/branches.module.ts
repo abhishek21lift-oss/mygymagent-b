b2db714f7a227ab81abf0073885be737b74a902c
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-20 07:37:40 -0700

    fix: register billing limits module dependencies

--- src/branches/branches.module.ts
+++ src/branches/branches.module.ts
@@ -4,6 +4,7 @@ import { BranchesController } from './branches.controller';
 import { BranchesService } from './branches.service';
 
 @Module({
+  imports: [PlatformBillingModule],
   controllers: [BranchesController],
   providers: [BranchesService],
   exports: [BranchesService],
