17f5f4d8cbeade540656996fb932e73657f75a2e
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-20 04:16:01 -0700

    fix: format completion backend files

--- src/platform-billing/platform-billing.module.ts
+++ src/platform-billing/platform-billing.module.ts
@@ -1,5 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PlatformBillingController } from './platform-billing.controller';
 import { PlatformBillingService } from './platform-billing.service';
-@Module({ controllers: [PlatformBillingController], providers: [PlatformBillingService], exports: [PlatformBillingService] })
+
+@Module({
+  controllers: [PlatformBillingController],
+  providers: [PlatformBillingService],
+  exports: [PlatformBillingService],
+})
 export class PlatformBillingModule {}
