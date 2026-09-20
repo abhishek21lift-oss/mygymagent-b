4c94250553498e067537bb2e12a903be3ade4748
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-20 07:36:43 -0700

    feat: wire billing limits into data module

--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { PlatformBillingModule } from '../platform-billing/platform-billing.module';
 import { DataController } from './data.controller';
 import { DataService } from './data.service';
 
