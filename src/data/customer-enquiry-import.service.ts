990ff484d6e0a1a2a12c3373ec3ce608465c03eb
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-21 16:00:06 -0700

    fix(data): keep guarded importer lint compatible

--- src/data/customer-enquiry-import.service.ts
+++ src/data/customer-enquiry-import.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { PlatformBillingService } from '../platform-billing/platform-billing.service';
