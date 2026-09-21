5d4ac7268f406043686b1bfdf55b24e73f436e4f
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-21 14:15:43 -0700

    chore: keep communication module formatter-stable

--- src/communications/communications.module.ts
+++ src/communications/communications.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { CommunicationsController } from './communications.controller';
