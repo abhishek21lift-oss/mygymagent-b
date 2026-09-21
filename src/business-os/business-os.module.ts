22ee83dc5eadceed31d0fafa4587db7991f9f5ea
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-21 14:12:25 -0700

    chore: keep generated business OS files formatter-stable

--- src/business-os/business-os.module.ts
+++ src/business-os/business-os.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { BusinessOsController } from './business-os.controller';
 import { BusinessOsService } from './business-os.service';
