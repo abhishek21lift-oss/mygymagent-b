5a14a48fa0496d85e5b70871176cf23a46dad003
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-21 15:56:41 -0700

    feat(data): register guarded customer enquiry importer

--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PlatformBillingModule } from '../platform-billing/platform-billing.module';
 import { DataController } from './data.controller';
 import { DataService } from './data.service';
+import { CustomerEnquiryImportService } from './customer-enquiry-import.service';
 
 @Module({
   imports: [PlatformBillingModule],
   controllers: [DataController],
-  providers: [DataService],
+  providers: [DataService, CustomerEnquiryImportService],
   exports: [DataService],
 })
 export class DataModule {}
