d1404a6c0d2cdb185b8675a9b47d0a0d03128f8b
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-21 14:12:47 -0700

    chore: keep generated business OS files formatter-stable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
