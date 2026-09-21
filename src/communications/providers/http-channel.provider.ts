155d39dea7b8da97c1d68f1ad9225c141cab8b2f
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-21 14:12:29 -0700

    chore: keep generated business OS files formatter-stable

--- src/communications/providers/http-channel.provider.ts
+++ src/communications/providers/http-channel.provider.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ChannelNotConfiguredError } from '../interfaces/email-provider.interface';
