e2405b9cd56c6b355c8331762cd8afe74de40c9d
abhishek21lift-oss <abhishek21lift@gmail.com>
2026-09-21 21:22:12 -0700

    fix(notifications): make notification files lint-safe

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import type { Prisma } from '@prisma/client';
